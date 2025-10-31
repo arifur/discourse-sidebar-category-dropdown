171fc65a327a132e7ed8fc941435ae1200ea8dc7
arifur <mailarifur@gmail.com>
2025-10-31 23:07:56 +0600

    initial commit

---
+++ plugin.rb
@@ -0,0 +1,10 @@
+# name: discourse-sidebar-category-dropdown
+# about: Discourse Sidebar Category Dropdown 
+# version: 0.0.1
+# authors: Arifur Rahman
+# url: https://github.com/arifur/discourse-sidebar-category-dropdown.git
+
+
+after_initialize do
+
+end
